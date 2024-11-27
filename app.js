6fc987bfab4155a93c54ce0f8bc8ab50518105a5
Jimmy <Jimmlius923@gmail.com>
2024-11-27 16:22:42 +0100

    added validation examples

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { body, validationResult } from "express-validator";
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.get('/get-header', (req, res) => {
 // Handling Params
 app.get('/user/:id', (req, res) => {
     const userId = req.params.id;
-    
+
     // console.log(typeof(userId));
 
     // if (typeof(userId) != 'number') {
@@ -27,15 +28,15 @@ app.get('/user/:id', (req, res) => {
     // } else {
     // res.send(`User ID is ${userId}`);
     // };
- 
+
     res.send(`User ID is ${userId}`);
 });
 
 // Cookies
 app.use(cookieParser());
 app.get('/set-cookie', (req, res) => {
-    res.cookie('theme', 'dark', {maxAge: (10 * 60 * 1000), httpOnly: true}); // 10mins * 60secs * 1000ms
-    res.cookie('color', 'white', {maxAge: 9000000, httpOnly: true});
+    res.cookie('theme', 'dark', { maxAge: (10 * 60 * 1000), httpOnly: true }); // 10mins * 60secs * 1000ms
+    res.cookie('color', 'white', { maxAge: 9000000, httpOnly: true });
     res.send('2 cookies has been set');
 });
 
@@ -48,6 +49,73 @@ app.get('/clear-cookie', (req, res) => {
     res.clearCookie('theme');
     res.clearCookie('color');
     res.send('2 cookies cleared')
+})
+
+// Validation
+// app.use(express.json());
+
+// app.post('/submit', [
+//     body('email').isEmail().withMessage('Must be a valid email'),
+//     body('username').isLength({min: 5}).withMessage('Username must be letters and numbers between 5 - 15 characters')
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({errors: errors.array()})
+//     } 
+
+//     res.send('Data is valid');
+// })
+
+// // Validation Example 2
+// app.use(express.json());
+
+// app.post('/signup', [
+//     body('email').isEmail().withMessage('Please, provide a valid email address'),
+//     body('password').isLength({min: 5}).withMessage('Password must be at least 5 characters long.')
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({errors: errors.array()})
+//     }
+
+//     const {email, password} = req.body;
+//     res.send(`Your email ${email} has been successfully signed up`);
+// })
+
+// Validation Example 3
+app.use(express.json());
+
+let validator = [
+    body('username')
+        .notEmpty().withMessage('username cannot be empty')
+        .isLength({ max: 15 }).withMessage('Username must not be more than 15 characters')
+        .custom(value => {
+            if (!value.startsWith('JP')) {
+                throw new Error('Username must start with JP')
+            }
+            return true
+        }),
+    body('email')
+        .isEmail().withMessage('Invalid Email Format')
+        .normalizeEmail(),
+    body('password')
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+        .matches(/\d/).withMessage('Password must contain a number'),
+    body('age').custom(value => {
+        if (value < 18) {
+            throw new Error('Age must be at least 18')
+        }
+        return true
+        })          
+]
+app.post('/signup', validator, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, age, password } = req.body
+    res.send(`Dear ${username}, \nYou're ${age} years old. \nYour Signup for ${email} is successful! \nYou can now Proceed to login.`)
 })
 
 app.listen(3000, () => {
